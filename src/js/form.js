01d17fa3366fc2ad945eb1b195aa14df38e57ed3
Marakas <igorpetriv.bsc@gmail.com>
2025-03-03 22:09:52 +0200

    img link fixed

--- src/js/form.js
+++ src/js/form.js
@@ -5,10 +5,10 @@ const sendButton = document.querySelector('.form-button');
 sendButton.addEventListener('click', event => {
     event.preventDefault();
 
-    letterImg.src = "/img/closed-lttr.png";
+    letterImg.src = "/goit-js-hw-09/img/closed-lttr.png";
     letterImg.classList.add('letter-animation');
     setTimeout(() => {
-        letterImg.src = "/img/mail_10345769.png";
+        letterImg.src = "/goit-js-hw-09/img/mail_10345769.png";
         letterImg.classList.remove('letter-animation')
     }, 2000)
 })
