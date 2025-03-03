49c58403d157950c5cfb963cbb567332d56de1cd
Marakas <igorpetriv.bsc@gmail.com>
2025-03-03 20:58:24 +0200

    q

--- src/js/form.js
+++ src/js/form.js
@@ -4,10 +4,10 @@ const sendButton = document.querySelector('.form-button');
 sendButton.addEventListener('click', event => {
     event.preventDefault();
 
-    letterImg.src = '/img/closed-lttr.png';
+    letterImg.src = "/img/closed-lttr.png";
     letterImg.classList.add('letter-animation');
     setTimeout(() => {
-        letterImg.src = '/img/mail_10345769.png';
+        letterImg.src = "/img/mail_10345769.png";
         letterImg.classList.remove('letter-animation')
     }, 2000)
 })
